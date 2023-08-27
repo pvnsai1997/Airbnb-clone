f184dc8b413e55b14015e42c4808a1eeb79f8cce
Pavan Sai Gundapaneni <dornalasrinidhi785@gmail.com>
2023-08-27 17:08:24 -0400

    Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import SearchPage from './SearchPage';
 
 function App() {
-  return (
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        < Header />
+        <Routes>
+          {/* /*/}
+          {/* Search */}
+          <Route path='/Search' element={<SearchPage />} />
+
+          <Route path ="/" element={<Home></Home>} />
+
+        </Routes>
+
+      <Footer/>
+
+      </Router>
+
+      {/* Header */}
+      {
+        /* Bannerr  */
+      }
+
+
+      {
+        /* Cards*/
+      }
+
+      {
+        /* Footer */
+      } 
+
+      { /*  */}
     </div>
   );
 }
